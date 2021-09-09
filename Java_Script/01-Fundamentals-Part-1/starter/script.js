a5cbc6b2119febef3943c53079105d629032bbf6
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-09-09 22:15:27 +0700

    Value and variable

--- Java_Script/01-Fundamentals-Part-1/starter/script.js
+++ Java_Script/01-Fundamentals-Part-1/starter/script.js
@@ -4,3 +4,27 @@ if (js === "OMG") alert("Wow, this is so amazing.haha");
 
 console.log(10+10);
 
+// 4. Values and Variable
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
