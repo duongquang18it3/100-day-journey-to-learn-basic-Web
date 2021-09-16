b4ea652e250ef5fb086e970c42bafd476c3c31bb
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-09-16 16:16:08 +0700

    3.2 Acvating Strict Mode

---
+++ Java_Script/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+'use strict';
+// 3.2 Acvating Strict Mode ////////////////
+
+/* let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+const interface = 'Audio';
+const private = 534; */
