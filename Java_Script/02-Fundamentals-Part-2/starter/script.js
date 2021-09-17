fdae14ab0563503107fcfcbc87dfa62e28ca3218
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-09-17 10:49:19 +0700

    3.4 Function Declarations and Expressions

--- Java_Script/02-Fundamentals-Part-2/starter/script.js
+++ Java_Script/02-Fundamentals-Part-2/starter/script.js
@@ -25,3 +25,18 @@ const appleJuice = juiceProcessor(5, 0);
 console.log(appleJuice);
 const orangeJuice = juiceProcessor(0, 5);
 console.log(orangeJuice); */
+
+//////// 3.4 Function Declarations and Expressions /////
+/* // Declaration
+function calcAge1 (birthYeah){
+    return 2021 - birthYeah;
+};
+const age1 = calcAge1(1999);
+
+
+// Expressions
+const calcAge2 = function (birthYeah){
+    return 2021 - birthYeah;
+};
+const age2 = calcAge2(1999);
+console.log(age1, age2); */
