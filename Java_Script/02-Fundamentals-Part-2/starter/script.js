b3dfb516d1568beb0d9967f5bc52b3c32848a6e4
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-09-17 10:07:00 +0700

    3.3 Function

--- Java_Script/02-Fundamentals-Part-2/starter/script.js
+++ Java_Script/02-Fundamentals-Part-2/starter/script.js
@@ -9,3 +9,19 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 const interface = 'Audio';
 const private = 534; */
+
+//////////3.3 Function/////////////
+
+/* function logger(){
+    console.log("My name is Quang");
+}
+logger();
+
+function juiceProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice
+}
+const appleJuice = juiceProcessor(5, 0);
+console.log(appleJuice);
+const orangeJuice = juiceProcessor(0, 5);
+console.log(orangeJuice); */
