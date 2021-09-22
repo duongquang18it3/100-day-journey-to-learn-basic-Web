5d1f29162d55edeaa9dafb995373e393d2ad3b07
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-09-22 17:10:11 +0700

    3.17 Looping Backwards and Loops in Loops

--- Java_Script/02-Fundamentals-Part-2/starter/script.js
+++ Java_Script/02-Fundamentals-Part-2/starter/script.js
@@ -312,7 +312,7 @@ for ( let rep = 1; rep <= 10; rep++){
 } */
 ///////////////
 // 3.16 Looping Arrays, Breaking, and Continuing
-
+/* 
 const thuArray = [
     'Le',
     'Tuyet Thu',
@@ -350,4 +350,31 @@ console.log('------- ONLY STRING ------');
 for (let i = 0; i < thuArray.length; i++){
     if(typeof thuArray[i] === 'number') break;
     console.log(thuArray[i], ' is type: ' ,typeof thuArray[i]);
-}
+} */
+
+////////////////////////////////
+// 3.17 Looping Backwards and Loops in Loops
+/* 
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+  ];
+  
+  // 0, 1, ..., 4
+  // 4, 3, ..., 0
+  
+  for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+  }
+
+  for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`-------- Starting exercise ${exercise}`);
+  
+    for (let rep = 1; rep < 6; rep++) {
+      console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+    }
+  } */
