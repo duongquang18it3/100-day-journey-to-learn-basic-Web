aa8caa4981809cd9d4690b6ddde521dd6bc89042
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-09-26 16:02:37 +0700

    5.8 Debugging

--- Java_Script/03-Developer-Skills/starter/script.js
+++ Java_Script/03-Developer-Skills/starter/script.js
@@ -1,4 +1,22 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+/* 'use strict';
 const calcAge = birthYear => 2037 - birthYear;
 
+console.log("Hi");
+console.log("hui"); */
+
+////////////////////////////
+// 5.8 Debugging with the Console and Breakpoint
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // value: Number(prompt('Degrees celsius...'))
+    value: 100
+  };
+  console.table(measurement);
+
+  const kelvin = measurement.value + 366;
+  return kelvin;
+};
+console.log(measureKelvin());
