60fb929671761773a06e8dbeaa5d709c6066984d
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-09-29 15:52:06 +0700

    7.5 Handling Click Events

--- Java_Script/05-Guess-My-Number/starter/script.js
+++ Java_Script/05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-// Selecting and Manipulating Elements
+/* // Selecting and Manipulating Elements
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉🎉 Correct number!';
 
@@ -8,5 +8,14 @@ document.querySelector('.score').textContent = 10;
 
 //input filed
 document.querySelector('.guess').value = 16;
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value); */
 
+//7.5 Handling Click Events
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number haha';
+  } 
+});
