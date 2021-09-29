0f044c90e52a6ba8e152b36ee1f58fc285727816
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-09-29 17:11:22 +0700

    7.6 Implementing the Game Logic

--- Java_Script/05-Guess-My-Number/starter/script.js
+++ Java_Script/05-Guess-My-Number/starter/script.js
@@ -12,10 +12,39 @@ console.log(document.querySelector('.guess').value); */
 
 //7.5 Handling Click Events
 
+//7.6 Implementing the Game Logic
+const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+// document.querySelector('.number').textContent = number;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = 'No number haha';
-  } 
+  } else if (guess < number) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = ' Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' You lost the game';
+    }
+  } else if (guess > number) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = ' Too hight  ';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' You lost the game';
+    }
+  } else {
+    document.querySelector('.message').textContent = 'Correct Number !';
+    document.querySelector('.number').textContent = number;
+    document.querySelector('.highscore').textContent = score;
+  }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.guess').value = '';
 });
