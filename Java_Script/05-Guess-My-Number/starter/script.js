50ffab865bc2526c02a9857f9b232bd72026d509
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-09-29 14:07:15 +0700

    7.4 Selecting and Manipulating Elements

--- Java_Script/05-Guess-My-Number/starter/script.js
+++ Java_Script/05-Guess-My-Number/starter/script.js
@@ -1,2 +1,12 @@
 'use strict';
+// Selecting and Manipulating Elements
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉🎉 Correct number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+//input filed
+document.querySelector('.guess').value = 16;
+console.log(document.querySelector('.guess').value);
+
