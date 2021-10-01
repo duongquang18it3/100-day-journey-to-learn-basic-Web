4500ea6a88483bf615af58fc3dd455c5a3f94070
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-01 14:35:42 +0700

    Close modal with both of clode btn and overlay

--- Java_Script/06-Modal/starter/script.js
+++ Java_Script/06-Modal/starter/script.js
@@ -5,9 +5,17 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsShowModal);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const close = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 for (let i = 0; i < btnsShowModal.length; i++)
-  btnsShowModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsShowModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', close);
+overlay.addEventListener('click', close);
