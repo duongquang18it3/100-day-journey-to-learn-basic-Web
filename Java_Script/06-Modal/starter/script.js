7af596d61fdb2a94c100895f03149ff6d6b8dee5
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-01 11:40:09 +0700

    Working With Classes

--- Java_Script/06-Modal/starter/script.js
+++ Java_Script/06-Modal/starter/script.js
@@ -5,6 +5,9 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsShowModal.length);
+console.log(btnsShowModal);
 for (let i = 0; i < btnsShowModal.length; i++)
-  console.log(btnsShowModal[i].textContent);
+  btnsShowModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
