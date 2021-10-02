0f53052758ea97db11459413114c9f601de1e908
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-02 10:38:30 +0700

    Reseting Game

--- Java_Script/07-Pig-Game/final/script.js
+++ Java_Script/07-Pig-Game/final/script.js
@@ -28,10 +28,10 @@ const init = function () {
   current1El.textContent = 0;
 
   diceEl.classList.add('hidden');
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
   player0El.classList.add('player--active');
   player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
 };
 init();
 
@@ -57,9 +57,8 @@ btnRoll.addEventListener('click', function () {
     if (dice !== 1) {
       // Add dice to current score
       currentScore += dice;
-      document.getElementById(
-        `current--${activePlayer}`
-      ).textContent = currentScore;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
     } else {
       // Switch to next player
       switchPlayer();
