477367a0c1b8e7af55870decfc6db792cb62d5e2
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-06 15:33:53 +0700

    Destructuring Arrays

--- Java_Script/09-Data-Structures-Operators/starter/script.js
+++ Java_Script/09-Data-Structures-Operators/starter/script.js
@@ -2,7 +2,7 @@
 
 ///////////////////////////////
 // 9.2 Destructuring Arrays
-const restaurant = {
+/* const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
@@ -42,12 +42,16 @@ const nested = [2, 4, [5, 6]];
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
 const [p, q, r=1] = [8, 9];
-console.log(p, q, r);
+console.log(p, q, r); */
 /* // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+*/
+
+
+//Destructuring Objects
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -69,4 +73,9 @@ const restaurant = {
       close: 24,
     },
   },
-}; */
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  }
+}; 
+
+
