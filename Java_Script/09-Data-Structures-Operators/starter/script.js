8dc8795ab8950a8d34b001ca88ade5f3c9eb80f1
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-06 10:26:01 +0700

    9.2 Destructuring Arrays

--- Java_Script/09-Data-Structures-Operators/starter/script.js
+++ Java_Script/09-Data-Structures-Operators/starter/script.js
@@ -1,6 +1,49 @@
 'use strict';
 
-// Data needed for a later exercise
+///////////////////////////////
+// 9.2 Destructuring Arrays
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+const arr = [2, 3, 4];
+
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+console.log(a, b, c);
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , second] = restaurant.categories;
+console.log(main, second);
+
+// const temp = main;
+// main = second;
+// second = temp;
+// console.log(main, second);
+
+// Reverse
+[main, , second] = [second, , main];
+console.log(main, second);
+// Receive 2 return values from a function;
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+const [p, q, r=1] = [8, 9];
+console.log(p, q, r);
+/* // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
@@ -26,4 +69,4 @@ const restaurant = {
       close: 24,
     },
   },
-};
+}; */
