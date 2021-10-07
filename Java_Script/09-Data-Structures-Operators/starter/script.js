46bc92dfad63aa817d56cfe7703c856e4bb582c8
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-07 15:34:17 +0700

    Rest pattern and parameters.

--- Java_Script/09-Data-Structures-Operators/starter/script.js
+++ Java_Script/09-Data-Structures-Operators/starter/script.js
@@ -114,7 +114,7 @@ const {
 console.log(open, close); */
 
 // The Spread Operator
-const restaurant = {
+/* const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
@@ -196,4 +196,84 @@ const restaurantCopy = { ...restaurant };
 console.log(restaurantCopy);
 restaurantCopy.name = 'Thu Quang Restaurant';
 console.log(restaurantCopy.name);
-console.log(restaurant.name);
+console.log(restaurant.name); */
+
+// Rest pattern and parameters.
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function ({ starterIndex = 1, mainIndex = 2, time, address }) {
+    console.log(`Order received ! ${this.starterMenu[starterIndex]} 
+    and ${this.mainMenu[mainIndex]} will be delivered to 
+     ${address} at ${time} `);
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicioius pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+  orderPizza: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
+};
+
+// Destructuring
+// Spread, because on Right of =
+//Unpacking
+const arr = [1, 2, ...[3, 4, 5]];
+console.log(arr);
+
+// // Rest, beacause on Left of =
+// // Packing
+const [a, b, ...orthers] = [1, 2, 3, 4, 5];
+console.log(a, b, orthers);
+
+const [pizza, , risotto, ...ortherFoods] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, ortherFoods);
+
+//Object
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// Functions
+const add = function (...number) {
+  let sum = 0;
+  for (let i = 0; i < number.length; i++) {
+    sum += number[i];
+  }
+  console.log(sum);
+};
+add(1, 2);
+add(3, 4, 5);
+add(7, 8, 9, 10);
+const x = [20, 30, 40];
+add(...x);
+
+restaurant.orderPizza('mushroom', 'chess', 'onion', 'olives');
+restaurant.orderPizza('mushroom');
