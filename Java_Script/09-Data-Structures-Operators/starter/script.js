232b2f99c442b62739d384e468557ddade688df0
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-08 14:25:06 +0700

    optional chaining Array and Object

--- Java_Script/09-Data-Structures-Operators/starter/script.js
+++ Java_Script/09-Data-Structures-Operators/starter/script.js
@@ -588,3 +588,20 @@ for (const day of days) {
   const open = restaurant.openingHours[day]?.open ?? 'closed';
   console.log(day, open);
 }
+
+//Check object
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisott?.(0, 1) ?? 'Method dose not exist');
+// Array
+const users = [
+  {
+    name: 'Quang',
+    age: 22,
+    email: 'quang@gmail.com',
+  },
+];
+// const users =[];
+console.log(users[0]?.name ?? 'No name');
+// -> Quang
+if (users.length > 0) console.log(users[0].name);
+else console.log('no name');
