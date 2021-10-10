011dd0de02e0f1dfd34451b7bd336c57af4f17b9
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-10 19:37:29 +0700

    9.17 Coding challenge 3

--- Java_Script/09-Data-Structures-Operators/starter/script.js
+++ Java_Script/09-Data-Structures-Operators/starter/script.js
@@ -949,3 +949,57 @@ console.log([...question.keys()]);
 console.log([...question.values()]);  
 
  */
+
+////////////////////////////////////////////
+// 9.17 Coding challenge 3
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that
+ happened (no duplicates)
+2. After the game has finished, is was found that the yellow 
+card from minute
+ 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened,
+ on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking 
+whether it's in the first half or second half (after 45 min) 
+of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+  ]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+console.log(
+  `An event happen, on average, every ${90/ gameEvents.size} minutes`
+);
+
+// 4.
+
+for(const [min, event] of gameEvents){
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+
