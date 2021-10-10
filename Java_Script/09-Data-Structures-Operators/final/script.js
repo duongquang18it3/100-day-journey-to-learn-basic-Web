504744962dacfcf950c681ff1b380ca66de7ed3a
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-10 16:55:02 +0700

    9.16 Maps: Iteration

--- Java_Script/09-Data-Structures-Operators/final/script.js
+++ Java_Script/09-Data-Structures-Operators/final/script.js
@@ -342,7 +342,8 @@ for (const [min, event] of gameEvents) {
 ///////////////////////////////////////
 // Maps: Iteration
 const question = new Map([
-  ['question', 'What is the best programming language in the world?'],
+  ['question', 'What is the best programming language 
+  in the world?'],
   [1, 'C'],
   [2, 'Java'],
   [3, 'JavaScript'],
--- Java_Script/09-Data-Structures-Operators/starter/script.js
+++ Java_Script/09-Data-Structures-Operators/starter/script.js
@@ -809,7 +809,7 @@ console.log(new Set('Jonasskjdnvksdnvk00').size); */
 // In Map we can have any type of key it can object, array, or other map
 
 // Create a empty Map we can use new Map();
-const rest = new Map();
+/* const rest = new Map();
 
 // Fill up map (add) use two argument
 // the first is key and the second is name of the key
@@ -856,4 +856,96 @@ console.log(rest.size);
 const arr = [1,2];
 rest.set(arr, 'Test');
 rest.set(document.querySelector('h1'), 'Heading');
-console.log(rest.get(arr));
+console.log(rest.get(arr)); */
+
+//////////////////////////////////////////////////////////
+// 9.16 Maps: Iteration
+
+/* const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // ES6 enhanced object literals
+  openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+// Create new Maps mutiple key and value (array in array) but don't use set
+
+const question = new Map([
+  ['question', 'What is the best programing language in the world ? '],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct ❤'],
+  [false, 'Try again !'],
+]);
+
+console.log(question);
+
+// Convert object to map
+console.log(openingHours);
+console.log(Object.entries(openingHours));
+
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// For loop Quizz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+const answer = 3;
+console.log(answer);
+// const answer = Number(prompt('Your answer'));
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);  
+
+ */
