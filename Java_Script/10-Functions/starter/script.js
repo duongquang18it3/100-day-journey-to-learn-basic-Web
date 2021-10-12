9ed631dfb6caa96aaad591a2c05d70613ce4d87c
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-12 21:01:14 +0700

    Defaulf Parameters

--- Java_Script/10-Functions/starter/script.js
+++ Java_Script/10-Functions/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+///////////////////////////////////////
+// Defaulf Parameters
+
+/* const bookings = [];
+const createBooking = function(
+    flightNum,
+    numPassengers = 1,
+    price = 199 * numPassengers
+){
+    const booking = {
+        flightNum,
+        numPassengers,
+        price,
+    }
+    console.log(booking);
+   
+};
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5); */
