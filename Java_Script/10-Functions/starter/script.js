f69c5db7a0c6ea673b85058b57f62492e4098460
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-14 16:52:55 +0700

    Function Accepting Callback Function

--- Java_Script/10-Functions/starter/script.js
+++ Java_Script/10-Functions/starter/script.js
@@ -61,3 +61,32 @@ const newPassport = function (person){
 };
 newPassport(thu);
 checkIn(flight,thu); */
+
+/////////////////////////////////////
+// Function Accepting Callback Function 
+
+const oneWord = function(str){
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str){
+  const [First, ...others] = str.split(' ');
+  return [First.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+
+const transformer = function(str, fn){
+  console.log(`Origianl string: ${str}`);
+  console.log(`Transfomer string: ${fn(str)}`);
+  console.log(`Transformer by: ${fn.name}`);
+}
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+  // JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
