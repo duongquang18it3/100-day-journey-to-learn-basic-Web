22d01947496eaed0b6b08065fec5306df29ab1be
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-22 10:34:44 +0700

    edit code coding challenge

--- Java_Script/11-Arrays-Bankist/starter/script.js
+++ Java_Script/11-Arrays-Bankist/starter/script.js
@@ -304,15 +304,15 @@ const max = movements.reduce((acc, mov) => {
   if (acc > mov) return acc;
   else return mov;
 }, movements[0]);
-console.log(max); */
+console.log(max); 
 
 const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   const adults = humanAges.filter(age => age >= 18);
   console.log(humanAges);
   console.log(adults);
-
-  // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+*/
+// const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
 ///////////////////////////////////////
 // Coding Challenge #2
 
@@ -331,16 +331,16 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
-
-  const average = adults.reduce(
-    (acc, age, i, arr) => acc + age / arr.length,
-    0
-  );
-
-  // 2 3. (2+3)/2 = 2.5 === 2/2+3/2 = 2.5
-
+/* const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(humanAges);
+  console.log(adults);
+  const average = adults.reduce((acc, age, i , arr) => acc + age/arr.length,0);
   return average;
 };
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
-console.log(avg1, avg2);
+console.log(avg1, avg2); */
+
+//////////////////////////////////////////////////
