7a651b6e436d5a219711de529292855540610468
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-26 21:00:57 +0700

    ctions and the new Operator

--- Java_Script/14-OOP/starter/script.js
+++ Java_Script/14-OOP/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+//////////////////////////////////////////////
+// 14.4 Constructor Functions and the new Operator
+const Person = function(firstName,birthYear){
+    // Instance properties
+    this.firtsName = firstName;
+    this.birthYear= birthYear;
+}
+const quang = new Person('Quang', 1999);
+console.log(quang);
+// 1. New {} is created
+// 2. function is called, this = {};
+// 3. linked to prototype
+// 4. function automatically return();
+
+const thu = new Person('Le', 1999);
+const jack = new Person('jack', 1999);
+console.log(thu, jack);
+
+console.log(thu instanceof Person);
