15481c9cb719cfc8644d0bbd4b0bb667a9937cf6
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-10-27 14:35:43 +0700

    Prototype

--- Java_Script/14-OOP/starter/script.js
+++ Java_Script/14-OOP/starter/script.js
@@ -1,11 +1,11 @@
 'use strict';
 //////////////////////////////////////////////
 // 14.4 Constructor Functions and the new Operator
-const Person = function(firstName,birthYear){
-    // Instance properties
-    this.firtsName = firstName;
-    this.birthYear= birthYear;
-}
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
 const quang = new Person('Quang', 1999);
 console.log(quang);
 // 1. New {} is created
@@ -13,8 +13,34 @@ console.log(quang);
 // 3. linked to prototype
 // 4. function automatically return();
 
-const thu = new Person('Le', 1999);
-const jack = new Person('jack', 1999);
+const thu = new Person('Thu', 1999);
+const jack = new Person('Jack', 1999);
 console.log(thu, jack);
 
 console.log(thu instanceof Person);
+
+//////////////////////////////////////
+// Prototype
+
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2021 - this.birthYear);
+};
+
+quang.calcAge();
+thu.calcAge();
+
+console.log(quang.__proto__);
+console.log(quang.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(quang));
+console.log(Person.prototype.isPrototypeOf(thu));
+
+// .prototyeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(quang.species, thu.species);
+
+console.log(quang.hasOwnProperty('firstName'));
+console.log(Person.prototype.hasOwnProperty('species'));
