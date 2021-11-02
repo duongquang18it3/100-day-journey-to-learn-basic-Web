a34c2a742548a1609da7c3d99eb91c9ccb131193
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-11-02 19:46:53 +0700

    16.3 Our First AJAX Call XMLHttpRequest

--- Java_Script/16-Asynchronous/starter/script.js
+++ Java_Script/16-Asynchronous/starter/script.js
@@ -4,3 +4,49 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+// 16.3 Our First AJAX Call XMLHttpRequest
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const key = Object.keys(data.currencies);
+    console.log(key);
+    const {
+      [key]: { name },
+    } = data.currencies;
+    console.log(name);
+
+    const html = `<article class="country">
+        <img class="country__img" src=${data.flags.png} />
+        <div class="country__data">
+          <h3 class="country__name">${data.name.common}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} milion people</p>
+          
+          <p class="country__row"><span>🗣️</span>${Object.values(
+            data.languages
+          )}</p>
+          <p class="country__row"><span>💰</span>
+          ${key}</p>
+          <p class="country__row"><span>💰</span>
+          ${name}
+          </p>
+        </div>
+        
+      </article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('vietnam');
+getCountryData('vietnam');
+
