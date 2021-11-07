d401f69cedb0abc6e424bf0b611191c89f1df3a4
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-11-07 08:53:22 +0700

    Coding challenge #2

--- Java_Script/16-Asynchronous/starter/script.js
+++ Java_Script/16-Asynchronous/starter/script.js
@@ -330,7 +330,7 @@ Promise.reject('Not OK').catch(res => console.log(res)); */
 ////////////////////////////////////////////////////
 // Promisifying the Geolocation API
 
-const getPosition = function () {
+/* const getPosition = function () {
   return new Promise(function (resolve, reject) {
     // navigator.geolocation.getCurrentPosition(
     //   position => resolve(position),
@@ -366,4 +366,51 @@ const whereAmI = function () {
       .catch(err => console.log(`${err.message} ❌❌❌ `));
   });
 };
-btn.addEventListener('click', whereAmI);
+btn.addEventListener('click', whereAmI); */
+
+//////////////////////////////////////////////////////////////
+// Coding challenge #2
+
+const wait = function (second) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, second * 1000);
+  });
+};
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
