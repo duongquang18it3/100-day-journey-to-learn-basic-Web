80797235d4ec82c518c1f35cd2c098b0a3677148
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2021-11-09 21:12:20 +0700

    CommonJS Modules

--- Java_Script/17-Modern-JS-Modules-Tooling/starter/script.js
+++ Java_Script/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -30,3 +30,17 @@ const ShoppingCart2 = (function () {
   ShoppingCart2.addToCart('pizza', 2);
   console.log(ShoppingCart2);
   console.log(ShoppingCart2.shippingCost);
+
+  //////////////////////////////////////
+// CommonJS Modules
+// Export
+export.addTocart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(
+    `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+  );
+};
+
+// Import
+const { addTocart } = require('./shoppingCart.js');
+*/
